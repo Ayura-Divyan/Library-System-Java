dcee856179c19e180361d461a4ee91298b222ca3
Ayura Divyan <ayuradivyan@gmail.com>
2026-04-21 21:44:00 +0530

    ISBN validation complete

---
+++ src/main/java/com/example/librarysystemjava/Validator.java
@@ -0,0 +1,36 @@
+package com.example.librarysystemjava;
+
+public class Validator {
+    // Validates the student ID
+    public static boolean isValidStudentId(String id){
+        return id != null && id.matches("\\d{8}");
+    }
+
+    // Validates the ISBN
+    public static boolean isValidIsbn(String isbn){
+        // Remove Hyphen and spaces
+        String cleanIsbn = isbn.replaceAll("\\s-", "");
+
+        // Length check
+        if(cleanIsbn.length() != 13 || !cleanIsbn.matches("\\d{13}")){
+            return false;
+        }
+
+        if (!cleanIsbn.startsWith("978") && !cleanIsbn.startsWith("979")) {
+            return false;
+        }
+
+        // Checksum Calculation
+        int total = 0;
+        for (int i = 0; i <= isbn.length(); i++) {
+            int digit = Character.getNumericValue(cleanIsbn.charAt(i));
+
+            if (i % 2 == 0) {
+                    total += digit;
+            } else {
+                total += digit * 3;
+            }
+        }
+        return total % 10 == 0;
+    }
+}
