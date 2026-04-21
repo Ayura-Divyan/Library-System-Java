333d66f26f5c7bc3b2ae822b65e3e8d3be6731e6
Ayura Divyan <ayuradivyan@gmail.com>
2026-04-21 18:38:57 +0530

    Transaction class complete

---
+++ src/main/java/com/example/librarysystemjava/Transaction.java
@@ -0,0 +1,61 @@
+package com.example.librarysystemjava;
+
+public class Transaction {
+    // Fields
+    private String transactionId;
+    private String date;
+    private String bookId;
+    private String studentId;
+    private int type;
+
+    // Constructor
+    public Transaction(String transactionId, String date, String bookId, String studentId, int type) {
+        this.transactionId = transactionId;
+        this.date = date;
+        this.bookId = bookId;
+        this.studentId = studentId;
+        this.type = type;
+    }
+
+    // Getters
+    public String transactionId() {
+        return transactionId;
+    }
+
+    public String date() {
+        return date;
+    }
+
+    public String bookId() {
+        return bookId;
+    }
+
+    public String studentId() {
+        return studentId;
+    }
+
+    public int type() {
+        return type;
+    }
+
+    // Setters
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
