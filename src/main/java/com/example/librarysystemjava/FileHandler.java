7963ceebce8fde9a797992b9867eccf0f39d9a17
Ayura Divyan <ayuradivyan@gmail.com>
2026-04-22 18:45:41 +0530

    Valid data  list added

---
+++ src/main/java/com/example/librarysystemjava/FileHandler.java
@@ -0,0 +1,13 @@
+package com.example.librarysystemjava;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileHandler {
+    // Valid data lists
+    private List<Book> validBooks =  new ArrayList<>();
+    private List<Student> validStudents =  new ArrayList<>();
+    private List<Transaction> validTransactions =  new ArrayList<>();
+
+    // Invalid data lists
+}
