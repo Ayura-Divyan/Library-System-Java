3628cc4fb4735602bf6a6bd13abc175d4a38b405
Ayura Divyan <ayuradivyan@gmail.com>
2026-04-23 13:18:15 +0530

    Basic controller set up complete

--- src/main/java/com/example/librarysystemjava/MainController.java
+++ src/main/java/com/example/librarysystemjava/MainController.java
@@ -1,14 +1,42 @@
 package com.example.librarysystemjava;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 
 public class MainController {
+
+    // Scene builder elements
     @FXML
-    private Label welcomeText;
+    private Button btnImport;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button btnReport;
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private StackPane contentArea;
+
+    @FXML
+    public void initialize() {
+        System.out.println("Launcher initialized");
     }
+
+    @FXML
+    public void onImportClicked() {
+        System.out.println("Import button clicked");
+    }
+
+    @FXML
+    public void onReportClicked() {
+        System.out.println("Export button clicked");
+    }
+
+    @FXML
+    public void onSearchClicked() {
+        System.out.println("Search button clicked");
+    }
+
 }
