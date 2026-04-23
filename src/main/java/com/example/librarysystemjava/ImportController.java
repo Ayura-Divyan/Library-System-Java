7bf25e2a70eba17e76452cf71a5ee064f19881ee
Ayura Divyan <ayuradivyan@gmail.com>
2026-04-24 00:28:14 +0530

    Import controller setup re-done

--- src/main/java/com/example/librarysystemjava/ImportController.java
+++ src/main/java/com/example/librarysystemjava/ImportController.java
@@ -3,6 +3,8 @@ package com.example.librarysystemjava;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import java.io.File;
 
 import java.awt.event.ActionEvent;
 
@@ -26,23 +28,38 @@ public class ImportController {
 
     @FXML
     public void onLoadClicked() {
-        System.out.println("Loading CSV File...");
+        System.out.println("Opening File Chooser...");
 
-        String booksPath = "data/books.csv";
-        String studentsPath = "data/students.csv";
-        String transactionsPath = "data/transactions.csv";
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV Files", "*.csv"));
 
-        fileHandler.loadBooks(booksPath);
-        fileHandler.loadStudents(studentsPath);
-        fileHandler.loadTransactions(transactionsPath);
+        // Select Book CSV file
+        fileChooser.setTitle("Select Books File");
+        File studentFile = fileChooser.showOpenDialog(null);
+
+        // Select Student CSV file
+        fileChooser.setTitle("Select Student File");
+        File bookFile = fileChooser.showOpenDialog(null);
+
+        //Select Transaction CSV file
+        fileChooser.setTitle("Select Transaction File");
+        File transactionFile = fileChooser.showOpenDialog(null);
+
+        if (bookFile != null && studentFile != null && transactionFile != null) {
+            fileHandler.loadBooks(bookFile.getAbsolutePath());
+            fileHandler.loadStudents(studentFile.getAbsolutePath());
+            fileHandler.loadTransactions(transactionFile.getAbsolutePath());
 
 
-        invalidRecordsList.getItems().clear();
-        invalidRecordsList.getItems().addAll(fileHandler.getInvalidBooks());
-        invalidRecordsList.getItems().addAll(fileHandler.getInvalidStudents());
-        invalidRecordsList.getItems().addAll(fileHandler.getInvalidTransactions());
+            invalidRecordsList.getItems().clear();
+            invalidRecordsList.getItems().addAll(fileHandler.getInvalidBooks());
+            invalidRecordsList.getItems().addAll(fileHandler.getInvalidStudents());
+            invalidRecordsList.getItems().addAll(fileHandler.getInvalidTransactions());
 
-        System.out.println("Files loaded! Invalid records found: " + invalidRecordsList.getItems().size());
+            System.out.println("Files loaded! Invalid records found: " + invalidRecordsList.getItems().size());
+        } else {
+            System.out.println("Import cancelled. You must select all three files");
+        }
     }
 
     @FXML
