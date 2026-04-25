ae19282cdb886da792e85662b2e3064d5cd8230b
Ayura Divyan <ayuradivyan@gmail.com>
2026-04-25 13:56:21 +0530

    Export logic complete

--- src/main/java/com/example/librarysystemjava/SearchController.java
+++ src/main/java/com/example/librarysystemjava/SearchController.java
@@ -6,7 +6,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 public class SearchController {
@@ -70,4 +75,36 @@ public class SearchController {
         System.out.println("Search Table Loaded\n\"Found \" + searchTable.getItems().size() + \" matching books.\"");
     }
 
+    @FXML
+    public void onExportClicked() {
+        if (searchTable.getItems().isEmpty()) {
+            System.out.println("Nothing to export");
+            return;
+        }
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Export Search Results");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+
+        fileChooser.setInitialFileName("book_search_results.txt");
+
+        File file = fileChooser.showSaveDialog(null);
+
+        if (file != null) {
+            try (PrintWriter writer = new PrintWriter(new FileWriter(file))) {
+                writer.println("--- Library System Book Search Results ---");
+                writer.println(String.format("%-10s | %-30s | %-6s | %-12s | %-10s", "Book ID", "Title", "Copies", "Availability", "Price"));
+                writer.println("----------------------------------------------------------------------------------");
+
+                for (Book book : searchTable.getItems()) {
+                    writer.println(String.format("%-10s | %-30s | %-6d | %-12d | £%.2f",
+                            book.getBookId(), book.getTitle(), book.getCopies(), book.getAvailability(), book.getPrice()));
+                }
+
+                System.out.println("Results successfully exported to " + file.getAbsolutePath());
+            } catch (IOException e) {
+                System.out.println("Error writing to file" + e.getMessage());
+            }
+        }
+    }
 }
