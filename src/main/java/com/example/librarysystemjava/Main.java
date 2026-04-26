a5aae36a23be7cd71c0f24889bb8cdbeeb315913
Ayura Divyan <ayuradivyan@gmail.com>
2026-04-26 23:52:08 +0530

    setTitle fixed

--- src/main/java/com/example/librarysystemjava/Main.java
+++ src/main/java/com/example/librarysystemjava/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
-        stage.setTitle("Library Systemx");
+        stage.setTitle("Library System");
         stage.setScene(scene);
         stage.show();
     }
